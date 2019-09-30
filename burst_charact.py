e5950e3e2365b28ed243d8e7038104b18dcfe074
aligarpe <aliciagp96@gmail.com>
2019-09-30 16:43:58 +0200

    Removed comments from burst_charact (old code) and new folder with test results

--- burst_charact.py
+++ burst_charact.py
@@ -34,19 +34,22 @@ N3t = 'N3t'
 #####################
 ### Loads file from model neuron
 
+# path = ""
+path = "test2/"
+
 N1m = 'N1m'
-N1m_data = read_model_burst(N1m)
+N1m_data = read_model_burst(path+N1m)[:-1]
 N1m_interv = analyse(N1m_data,N1m)
 
 
 N2v = 'N2v'
-N2v_data = read_model_burst(N2v)
+N2v_data = read_model_burst(path+N2v)[:-1]
 N2v_interv = analyse(N2v_data,N2v)
 
 
 
 N3t = 'N3t'
-N3t_data = read_model_burst(N3t)
+N3t_data = read_model_burst(path+N3t)[1:]
 N3t_interv = analyse(N3t_data,N3t)
 
 
@@ -59,67 +62,20 @@ N3t_interv = analyse(N3t_data,N3t)
 
 #####################
 
-###################################################
-######## 2 burst plot 
-################################################
-# plt.subplot(2,3,1)
-# plt.title("Burst duration" + neuron1)
-# plt.hist(dur1,rwidth=0.4)
-# plt.xlabel("Time (s)")
-
-
-# plt.subplot(2,3,2)
-# plt.title("Burst Interval" + neuron1)
-# plt.hist(ibi1,rwidth=0.4)
-# plt.xlabel("Time (s)")
-
-# plt.subplot(2,3,3)
-# plt.title("Burst Period" + neuron1)
-# plt.hist(per1,rwidth=0.4)
-# plt.xlabel("Time (s)")
-
-
-# plt.subplot(2,3,4)
-# plt.title("Burst duration" + neuron1)
-# plt.hist(dur2,rwidth=0.4)
-# plt.xlabel("Time (s)")
-
-
-# plt.subplot(2,3,5)
-# plt.title("Burst Interval" + neuron1)
-# plt.hist(ibi2,rwidth=0.4)
-# plt.xlabel("Time (s)")
-
-# plt.subplot(2,3,6)
-# plt.title("Burst Period" + neuron1)
-# plt.hist(per2,rwidth=0.4)
-# plt.xlabel("Time (s)")
-
-# plt.show()
-
-####################################
-######## STATS #####################
-####################################
-
-
-
 
 ####################################
 ########  CORRELATIONS  ############
 ####################################
 
-# plot_corr(char1[PER],char2[IBI],"Period","Duration of "+neuron2)
-# plot_corr(char2[PER],char2[PER],"Period","Duration")
-
 period = N1m_interv[PER]
 
-
-ran = (0.8,0.9)
+plt.figure(figsize=(30,5))
+ran = (0,0.1)
 plt.subplot(1,3,1)
 # plt.ylim(0.1,1.2)
 plt.ylim(ran)
 plt.xlim(ran)
-plot_corr(period,N1m_interv[DUR][:-1],"Period","Duration of "+N1m,False)
+plot_corr(period,N1m_interv[DUR],"Period","Duration of "+N1m,False)
 plt.subplot(1,3,2)
 # plt.ylim(0.1,1.2)
 plt.ylim(ran)
