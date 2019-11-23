121dc6912b3b7c8bade26304f2a622da207c89e4
Alicia <aliciagp96@gmail.com>
2019-11-23 14:27:11 +0100

    move autom

---
+++ move_elems.py
@@ -0,0 +1,17 @@
+
+import os,sys
+
+if len(sys.argv) > 2:
+	path1 = sys.argv[1]
+	path2 = sys.argv[2]
+	# file = sys.argv[2]
+else:
+	path = ''
+	# file  = 'test-spikes.asc'
+
+dest = path2 + "selection/"
+os.system("mkdir -p "+dest)
+
+for f in os.listdir(path1):
+	if f.endswith(".png"):
+		os.system("cp "+path1+f+" "+dest+f[:-3]+"asc")
