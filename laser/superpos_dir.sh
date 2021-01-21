3b69eb8d3665f199faf1450e2932d90bbe937d99
aligarpe <aliciagp96@gmail.com>
2021-01-21 14:08:58 +0100

    New superpos automatization for one whole directory (superpos_dir), renamed last superpos_dir to superpos_dirs

--- laser/superpos_dir.sh
+++ laser/superpos_dir.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 
-for dir in $1/*/     # list directories in the form "/tmp/dirname/"
-do
-    dir=${dir%*/}      # remove the trailing "/"
-    # echo ${dir##*/}    # print everything after the final "/"
-    dir_name=${dir##*/}
-	line=$(head -n 1 $1/$dir_name/run_info.txt)
-    # echo $1/$dir_name $line
-	python3 autom_superpos.py ../../data/laser/single_neuron/$dir_name $line ;
-done
+dir=$1
+
+dir=${dir%*/}      # remove the trailing "/"
+echo ${dir##*/}    # print everything after the final "/"
+dir_name=${dir##*/}
+line=$(head -n 1 $1/run_info.txt)
+# echo $1/$dir_name $line
+python3 autom_superpos.py ../../data/laser/single_neuron/$dir_name $line ;
