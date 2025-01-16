1cb28636d7b06a32792e0f0b35a585157e05784a
Alicia Garrido-Peña <aliciagp96@gmail.com>
2025-01-16 10:52:03 +0100

    Two plots only average and all;   column selected by config file

--- laser/superpos_burst_waveform.py
+++ laser/superpos_burst_waveform.py
@@ -6,7 +6,7 @@ import pickle as pkl
 import matplotlib.pyplot as plt
 
 
-colors= ['cornflowerblue','indianred','yellowgreen']
+colors= ['cornflowerblue','crimson','yellowgreen']
 # --- MAIN FUNCTION ---
 def main(config_file_path, data_frame_path):
     """
@@ -26,22 +26,32 @@ def main(config_file_path, data_frame_path):
     print(df)
 
     triplets = [triplet.split() for triplet in config['Superposition']['triplets'].split('|')]
+    column_id = int(config['Superposition']['column_id'])
     print(triplets)
 
     cols = len(triplets)//2
-    fig, ax = plt.subplots(2, cols, figsize=(12, 6))
+    fig, ax = plt.subplots(2, cols, figsize=(12, 8))
     ax = ax.flatten()
+
+    fig_mean, ax_mean = plt.subplots(2, cols, figsize=(12, 8))
+    ax_mean = ax_mean.flatten()
+
+    title = data_frame_path[data_frame_path.find('/')+1:
+                            data_frame_path.rfind('_extended_data.pkl')]
     for i, triplet in enumerate(triplets):
         for j, trial in enumerate(triplet):
             trial = int(trial)
-            waveforms = df.loc[df['Trial'] == trial, 'Waveforms'].values[0]
+            waveforms = df.loc[(df['Trial'] == trial) & 
+                               (df['Column_id'] == column_id),
+                               'Waveforms'].values[0]
             if j == 1:
                 label = df.loc[df['Trial'] == trial, 'Type'].values[0]
             else:
                 label = 'control' if i == 0 else 'recovery'
 
             # Compute average waveform
-            try:    
+            try:
+                # TODO align to first spike
                 aligned_waveforms = np.array([w-min(w) for w in waveforms])
                 w_mean = np.mean(aligned_waveforms, axis=0)
                 # w_mean -= np.max(w_mean)
@@ -50,14 +60,26 @@ def main(config_file_path, data_frame_path):
                 continue
 
             # Plot average and traces
-            ax[i].plot(aligned_waveforms, color=colors[j], label=label,
+            ax[i].plot(aligned_waveforms.T, color=colors[j], label=label,
                        linewidth=0.01)
-            ax[i].plot(w_mean, color=colors[j], label=label)
+            ax[i].plot(w_mean.T, color=colors[j], label=label)
+            ax_mean[i].plot(w_mean.T, color=colors[j], label=label)
             if j == 1:
                 ax[i].set_title(label)
+                ax_mean[i].set_title(label)
 
-    plt.tight_layout()
+    fig.suptitle(title)    
+    fig_mean.suptitle(title)    
+
+    fig.tight_layout()
+    fig_mean.tight_layout()
+    
+    print("Saving at", config_file_path[:-4]+'_'+title+'.png')
+    fig.savefig(config_file_path[:-4]+'_'+title+'.png', dpi=200, format='png')
+    fig_mean.savefig(config_file_path[:-4]+'_'+title+'_average.pdf', dpi=200, format='pdf')
+
     plt.show()
+
     exit()
 
     waveforms_f = config['Superposition']['waveform_files']
