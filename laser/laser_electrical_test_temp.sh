92c4a2631d069f08e2e304d994da0f8639630ed7
Alicia Garrido-Peña <aliciagp96@gmail.com>
2025-03-01 13:47:14 +0100

    laser autom to laser

--- laser/laser_electrical_test_temp.sh
+++ laser/laser_electrical_test_temp.sh
@@ -1,23 +1,22 @@
+python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/exp1_control_RPD2_waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/exp1_control_VD1_waveform.txt -w 30 -l1 "RPD2 control" -l2 "VD1 control" -c2 r -ti "RPD2 control VD1 control"
 
-python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_Control_RPD2_Waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_Control_VD1_Waveform.txt -w 30 -l1 "RPD2 Control" -l2 "VD1 Control" -c2 r -ti "RPD2 Control VD1 Control" -sh n
+python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/exp1_control_RPD2_waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/exp1_RPD2_laser_RPD2_waveform.txt -w 30 -l1 "RPD2 control" -l2 "RPD2 laser on RPD2" -c2 r -ti "RPD2 control-laser laser on RPD2"
 
-python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_Control_RPD2_Waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_RPD2_Laser_RPD2_Waveform.txt -w 30 -l1 "RPD2 Control" -l2 "RPD2 Laser on RPD2" -c2 r -ti "RPD2 Control-Laser Laser on RPD2" -sh n
+python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/exp1_control_VD1_waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/exp1_RPD2_laser_VD1_waveform.txt -w 30 -l1 "VD1 control" -l2 "VD1 laser" -c2 r -ti "VD1 control-laser laser on RPD2"
 
-python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_Control_VD1_Waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_RPD2_Laser_VD1_Waveform.txt -w 30 -l1 "VD1 Control" -l2 "VD1 Laser" -c2 r -ti "VD1 Control-Laser Laser on RPD2" -sh n
+python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/exp1_RPD2_laser_RPD2_waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/exp1_RPD2_laser_VD1_waveform.txt -w 30 -l1 "RPD2 laser on RPD2" -l2 "VD1 laser on RPD2" -c2 r -ti "RPD2 VD1 laser laser on RPD2"
 
-python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_RPD2_Laser_RPD2_Waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_RPD2_Laser_VD1_Waveform.txt -w 30 -l1 "RPD2 Laser on RPD2" -l2 "VD1 Laser on RPD2" -c2 r -ti "RPD2 VD1 Laser Laser on RPD2" -sh n
+python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/exp1_control_RPD2_waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/exp1_gap_laser_RPD2_waveform.txt -w 30 -l1 "RPD2 control" -l2 "RPD2 laser on gap" -c2 r -ti "RPD2 control-laser laser on gap"
 
-python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_Control_RPD2_Waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_Gap_Laser_RPD2_Waveform.txt -w 30 -l1 "RPD2 Control" -l2 "RPD2 Laser on Gap" -c2 r -ti "RPD2 Control-Laser Laser on Gap" -sh n
+python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/exp1_control_VD1_waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/exp1_gap_laser_VD1_waveform.txt -w 30 -l1 "VD1 control" -l2 "VD1 laser on gap" -c2 r -ti "VD1 control-laser laser on gap"
 
-python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_Control_VD1_Waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_Gap_Laser_VD1_Waveform.txt -w 30 -l1 "VD1 Control" -l2 "VD1 Laser on Gap" -c2 r -ti "VD1 Control-Laser Laser on Gap" -sh n
+python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/exp1_gap_laser_RPD2_waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/exp1_gap_laser_VD1_waveform.txt -w 30 -l1 "RPD2 laser on gap" -l2 "VD1 laser on gap" -c2 r -ti "RPD2 VD1 laser laser on gap"
 
-python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_Gap_Laser_RPD2_Waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_Gap_Laser_VD1_Waveform.txt -w 30 -l1 "RPD2 Laser on Gap" -l2 "VD1 Laser on Gap" -c2 r -ti "RPD2 VD1 Laser Laser on Gap" -sh n
+python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/exp1_control_RPD2_waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/exp1_VD1_laser_RPD2_waveform.txt -w 30 -l1 "RPD2 control" -l2 "RPD2 laser on VD1" -c2 r -ti "RPD2 control-laser laser on VD1"
 
-python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_Control_RPD2_Waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_VD1_Laser_RPD2_Waveform.txt -w 30 -l1 "RPD2 Control" -l2 "RPD2 Laser on VD1" -c2 r -ti "RPD2 Control-Laser Laser on VD1" -sh n
+python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/exp1_control_VD1_waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/exp1_VD1_laser_VD1_waveform.txt -w 30 -l1 "VD1 control" -l2 "VD1 laser on VD1" -c2 r -ti "VD1 control-laser laser on VD1"
 
-python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_Control_VD1_Waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_VD1_Laser_VD1_Waveform.txt -w 30 -l1 "VD1 Control" -l2 "VD1 Laser on VD1" -c2 r -ti "VD1 Control-Laser Laser on VD1" -sh n
-
-python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_VD1_Laser_RPD2_Waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/$1/Exp1_VD1_Laser_VD1_Waveform.txt -w 30 -l1 "RPD2 Laser on VD1" -l2 "VD1 Laser on VD1" -c2 r -ti "RPD2 VD1 Laser Laser on VD1" -sh n
+python3 superpos_from_events.py -p1 ../../data/laser/electrical/26-May-2021/events/exp1_VD1_laser_RPD2_waveform.txt -p2 ../../data/laser/electrical/26-May-2021/events/exp1_VD1_laser_VD1_waveform.txt -w 30 -l1 "RPD2 laser on VD1" -l2 "VD1 laser on VD1" -c2 r -ti "RPD2 VD1 laser laser on VD1"
 
 
 
