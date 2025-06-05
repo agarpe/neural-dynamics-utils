65368d073766b0edcd4446ded5101b3536d87791
Alicia Garrido-Peña <aliciagp96@gmail.com>
2025-06-05 13:43:57 +0200

    removed useless code

--- laser/superpos_burst_waveform.py
+++ laser/superpos_burst_waveform.py
@@ -5,6 +5,8 @@ import configparser
 import pickle as pkl
 import matplotlib.pyplot as plt
 
+def plot_waveforms():
+
 
 colors= ['cornflowerblue','crimson','yellowgreen']
 # --- MAIN FUNCTION ---
@@ -81,54 +83,6 @@ def main(config_file_path, data_frame_path):
     fig_mean.savefig(config_file_path[:-4]+'_'+title+'_average.pdf', dpi=200, format='pdf')
 
     plt.show()
-
-    exit()
-
-    waveforms_f = config['Superposition']['waveform_files']
-    waveforms_f = [w for w in waveforms_f.split()]
-    print(waveforms_f)
-
-    title = config['Superposition']['title']
-
-    data_file = config['Superposition']['trace_dataframe']
-
-    # Load DataFrame
-    try:
-        df = pd.read_pickle(data_file)
-        print(f"DataFrame loaded successfully. Shape: {df.shape}")
-    except Exception as e:
-        print(f"Error loading DataFrame: {e}")
-        return
-
-    # Process or use the DataFrame as needed
-    print(df.head())
-
-    waveforms = {trial: pkl.load(open(wf, 'rb')) for trial, wf in zip(trials, waveforms_f)}
-
-
-    for i,w in enumerate(waveforms):
-        type_for_trial = df[df['Trial'] == w]['Type'].unique()[0]
-        print(type_for_trial)
-
-
-        try:
-            waveform = np.array([w-min(w) for w in waveforms[w]])
-            w_mean = np.mean(waveform, axis=0)
-            # w_mean -= np.max(w_mean)
-        except Exception as e:
-            print(e.args)
-            continue
-        
-        
-        plt.plot(waveform.T, color=colors[i], label=type_for_trial, linewidth=0.01) 
-        plt.plot(w_mean.T, color=colors[i], label=type_for_trial)
-
-    plt.title(title)
-    # plt.legend()
-    print("Saving at", config_file_path[:-4]+'_'+title+'.png')
-    plt.savefig(config_file_path[:-4]+'_'+title+'.png',dpi=200, format='png')
-
-    # plt.show()
     
 
 
