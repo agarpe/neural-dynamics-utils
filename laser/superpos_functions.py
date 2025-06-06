95e39e83aec8b11e88300f56b7018868f9654dfa
Alicia Garrido-Peña <aliciagp96@gmail.com>
2025-06-06 10:20:48 +0200

    Updated version of superpos_functions

--- laser/superpos_functions.py
+++ laser/superpos_functions.py
@@ -8,6 +8,15 @@ from colour import Color
 from scipy.signal import argrelmax, find_peaks, peak_widths
 
 
+
+def close_all_figs():
+	import matplotlib.pyplot as plt
+	figs = list(map(plt.figure, plt.get_fignums()))
+
+	for fig in figs:
+		# print(fig)
+		plt.close(fig)
+		
 def create_dataframe(dicts,prefixes):
 	if len(dicts) != len(prefixes):
 		print("Error creating dataframe, dicts and prefixes with diffrent shapes")
@@ -111,6 +120,8 @@ def get_spike_info(df_log,spike,dt,show_durations,spike_i,error_count):
 COLORS = {'b':['cyan','darkblue'],'r':['coral','maroon'],'g':['lime','darkgreen']}
 
 def parse_color(col,n_events=0):
+	if col is None:
+		return None, None, None, None
 	colors=None
 	if(isinstance(col,str)):
 	#set first and last spike colors
@@ -136,13 +147,15 @@ def align_spike(spike,width_ms_l,width_ms_r,dt,id_,mode='ini'):
 
 	# prepare spike
 	try:
+	# spike = np.array(spike)
+	# print(type(spike))
 		spike = center(spike,width_ms_l, width_ms_r,dt) #center spike from max
-		spike = no_drift(spike,dt) #adjust drift
+		# spike = no_drift(spike,dt) #adjust drift
 		spike = align_to(spike,mode, dt)
 		return spike
 
 	except:
-		print("skip ",id_)
+		print("----skip ",id_)
 		return []
 
 
@@ -188,6 +201,7 @@ def plot_events(events,col,tit,width_ms_l=50,width_ms_r=50,dt=0.1,df_log={},show
 
 	ax=0
 	col,fst_color,last_color,colors =  parse_color(col,len(events))
+
 
 	count =[0]
 	ax_fst,=plt.plot([],[])
@@ -413,6 +427,7 @@ def center(spike,width_ms_l, width_ms_r,dt=0.1):
 # 	spike voltage values
 
 def no_drift(spike,mode='first_min',dt=0.1):
+	print(np.array(spike))
 	if(spike.shape[0]!=0):
 		spike = spike[~np.isnan(spike)] 
 		mn = np.min(spike)
@@ -430,6 +445,7 @@ def no_drift(spike,mode='first_min',dt=0.1):
 
 def align_to(spike,mode='peak',dt=0.1,sec_wind=2.0):
 	# mode = 'peak'
+	spike = np.array(spike)
 	if(spike.shape[0]!=0):
 		if mode == 'min':
 			indx = np.argmin(spike)
@@ -527,26 +543,42 @@ def align_to(spike,mode='peak',dt=0.1,sec_wind=2.0):
 # 	tol difference tolerance im mV (lower than 2 fails) --> DEPRECATED
 # Return:
 #	(min_thres,max_thres)
-def get_spike_duration(spike,dt,tol=2, thres_val=0.5, max_dur = 5): 
+def get_spike_duration(spike,dt,tol=2, thres_val=0.5, max_dur = 5, v_scale=1, plot=False): 
 	spike = spike[~np.isnan(spike)]
 
-	peaks, properties = find_peaks(spike, prominence=1, width=20)
+	peaks, properties = find_peaks(spike, prominence=1*v_scale, width=20)
+	print(peaks)
 
 	#Warning: this may fail for not centered waveforms	
 	if len(peaks)>1: # in case spike has several peaks, gets mid one.
 		mid_peak = np.isclose(len(spike)//2, peaks, atol=10)
 		peaks = peaks[mid_peak]
+	
+	print(peaks)
 	results_half = peak_widths(spike, peaks, rel_height=thres_val)
 
-	# x=spike
-	# plt.plot(x)
-	# plt.plot(peaks, x[peaks], "x")
-	# plt.hlines(*results_half[1:], color="C2")
-	# plt.show()
+	print(results_half)
+	x=spike
+	if plot:
+		plt.figure()
+		plt.plot(x)
+		plt.hlines(results_half[1][0],results_half[2][0]*dt, results_half[3][0]*dt, color="C2")
+	plt.show()
+	try: 
 
+		duration_vals = np.array([results_half[2][0], results_half[3][0]])
+		th = results_half[1]
+	except Exception as e:
+		# print(e.args)
 
-	duration_vals = np.array([results_half[2][0], results_half[3][0]])
-	th = results_half[1]
+		# x=spike
+		# plt.figure()
+		# plt.plot(x)
+		# plt.plot(peaks, x[peaks], "x")
+		# plt.hlines(*results_half[1:], color="C2")
+		# plt.show()
+		duration_vals = np.array([])
+		th = 0
 
 	# plt.plot(duration_vals,(th,th),'.',markersize=20,color='r')
 	
@@ -556,6 +588,10 @@ def get_spike_duration(spike,dt,tol=2, thres_val=0.5, max_dur = 5):
 	else:
 		return (duration_vals[0]*dt,duration_vals[-1]*dt),th
 
+def get_spike_duration_value(waveform, dt, plot=False, v_scale=1):
+    dur_refs,th = get_spike_duration(waveform, dt, plot=plot, v_scale=v_scale)
+    duration = dur_refs[1]-dur_refs[0]
+    return duration
 
 
 # Description: 
@@ -628,11 +664,14 @@ def get_spike_amplitude(spike,dt):
 # dt: data time step
 # n_points: number of points around position to calculate slope
 # slope_position: where to calculate slope: defalult value, mid of spike.
-def get_slope(spike,dt,n_points=10, slope_position=0.5):
+def get_slope(spike,dt,n_points=10, slope_position=0.5, plot=False, v_scale=1):
 	spike = spike[~np.isnan(spike)] 
-	mid_ps,th = get_spike_duration(spike,dt,thres_val=slope_position)
+	mid_ps,th = get_spike_duration(spike,dt,thres_val=slope_position, v_scale=v_scale)
 	indx1 = int(mid_ps[0]/dt) #From ms to point ref
 	indx2 = int(mid_ps[1]/dt) #From ms to point ref
+
+	scale = int(0.1/dt) #TODO: fix repol points and mid+ in ms not points. 
+	n_points *= scale
 
 	# n_points = int(n_ms /dt)
 	n_ms = n_points*dt
@@ -651,9 +690,10 @@ def get_slope(spike,dt,n_points=10, slope_position=0.5):
 	# if slope1 < 7:
 		# print(slope1, slope2)
 
-	plt.plot(time,spike,color='k',alpha=0.2)
-	plt.plot([t1-n_ms,t1+n_ms], [spike[indx1-n_points],spike[indx1+n_points]],color='b',linewidth=1.3)
-	plt.plot([t2-n_ms,t2+n_ms], [spike[indx2-n_points],spike[indx2+n_points]],color='b',linewidth=1.3)
+	if plot:
+		plt.plot(time,spike,color='k',alpha=0.2)
+		plt.plot([t1-n_ms,t1+n_ms], [spike[indx1-n_points],spike[indx1+n_points]],color='b',linewidth=1.3)
+		plt.plot([t2-n_ms,t2+n_ms], [spike[indx2-n_points],spike[indx2+n_points]],color='b',linewidth=1.3)
 	# plt.show()
 
 	# print(slope1, slope2)
@@ -664,21 +704,36 @@ def get_slope(spike,dt,n_points=10, slope_position=0.5):
 
 	return (slope1,slope2)
 
-def get_slope2(spike,dt,n_points=10, slope_position=0.8, repol_points=60):
+def get_slope2(spike,dt,n_points=5, slope_position=0.8, repol_points=60, plot=False):
 	spike = spike[~np.isnan(spike)] 
 	mid_ps,th = get_spike_duration(spike,dt,thres_val=slope_position)
 	indx1 = int(mid_ps[0]/dt) #From ms to point ref
 	indx2 = int(mid_ps[1]/dt) #From ms to point ref
 	mx_value = np.max(spike) #maximum V value (spike)
-	t_max = np.argmax(spike) #maximum V value (spike)
-	t2 = mid_ps[1]+2
+	t_max = np.argmax(spike) #maximum V value (spike)	
+
+	scale = int(0.1/dt) #TODO: fix repol points and mid+ in ms not points. 
+	repol_points *= scale
+	n_points *= scale
+
+
+	t2 = mid_ps[1]+ 2
 	indx2 = int(t2/dt) #From ms to point ref
 
-	# n_points = int(n_ms /dt)
+	# # n_points = int(n_ms /dt)
+	# half1 = spike[:t_max]
+	# half2 = spike[t_max:]
+	# # print(n_points)
+	# n_points = int((n_points/100)*len(half1[np.argmin(half1):]))
+	# print(n_points)
+
 	n_ms = n_points*dt
 
 	slope1 = (spike[indx1+n_points]-spike[indx1-n_points])/(n_ms*2) 
 	# slope2 = (spike[indx2+n_points]-spike[indx2-n_points])/(n_ms*2)
+
+	if (t_max+repol_points) > len(spike):
+		repol_points = len(spike)-t_max-1
 
 	slope2 = (mx_value-spike[t_max+repol_points])/((t_max*dt) - (t_max+repol_points)*dt)
 
@@ -688,9 +743,10 @@ def get_slope2(spike,dt,n_points=10, slope_position=0.8, repol_points=60):
 
 	time = np.arange(spike.size) *dt
 
-	plt.plot(time,spike,color='k',alpha=0.2)
-	plt.plot([t1-n_ms,t1+n_ms], [spike[indx1-n_points],spike[indx1+n_points]],color='b',linewidth=1.3)
-	plt.plot([t_max*dt,(t_max+repol_points)*dt], [mx_value,spike[t_max+repol_points]],color='b',linewidth=1.3)
+	if plot:
+		# plt.plot(time,spike,color='k',alpha=0.2)
+		plt.plot([t1-n_ms,t1+n_ms], [spike[indx1-n_points],spike[indx1+n_points]],color='b',linewidth=1.3)
+		plt.plot([t_max*dt,(t_max+repol_points)*dt], [mx_value,spike[t_max+repol_points]],color='b',linewidth=1.3)
 	# plt.show()
 	# if slope1 < 7:
 		# print(slope1, slope2)
@@ -732,3 +788,57 @@ def get_slope_max(spike,dt):
 	slope2 = (mx_value-spike[indx2-1])/dt
 
 	return (slope1,slope2)
+
+
+def normalize_by(df, type, metrics=['duration','depolarization slope','repolarization slope','amplitude']):
+    control_means = df[df['type']==type].groupby('file').mean()
+    control_means = control_means[metrics]
+
+    control_stds = df[df['type']==type].groupby('file').std()
+    control_stds = control_stds[metrics]
+    # print(control_means)
+
+    for group in control_means.index:
+        # df.loc[df['file']==group,metrics] = df.loc[df['file']==group,metrics].apply(lambda x: abs(x)/abs(control_means.loc[group]), axis=1)
+        df.loc[df['file']==group, metrics] = df.loc[df['file']==group, metrics].apply(lambda x: x/control_means.loc[group], axis=1)
+        # df.loc[df['file']==group,metrics] = df.loc[df['file']==group, metrics].apply(lambda x: (abs(x)-abs(control_means.loc[group]))/control_stds.loc[group], axis=1)
+
+    return df
+
+
+
+def get_sample(df, n_spikes):
+    if n_spikes < 0:
+        return df, pd.DataFrame(columns=df.columns)
+    discard =df.groupby(['file', 'type']).filter(lambda x: len(x) < n_spikes).reset_index(drop=True)
+    df = df.groupby(['file', 'type']).filter(lambda x: len(x) >= n_spikes).reset_index(drop=True)
+
+    for file in discard.groupby('file').groups.keys():
+        df.drop(df.index[df['file']==file], inplace=True)
+
+    df = df.groupby(['file', 'type']).apply(pd.DataFrame.sample,n=n_spikes).reset_index(drop=True)
+
+    return df, discard
+
+
+def clean_spikes(df, metrics, z_error=0.1):
+	# Clean spikes with different shapes
+	org_std = df['duration'].std()
+
+	from scipy.stats import zscore
+	for col in metrics:
+	    df[col+'_std'] = df.groupby(['file','type'])[[col]].transform('std')
+	    df[col+'_z'] = df.groupby(['file','type'])[[col]].transform(zscore)
+
+	print("Number of spikes discarded:", df.groupby(['file','type']).count())
+
+	# z_error = 0.1
+
+	df_outliers = df[df.index.isin(df.loc[(df['duration_z'] >= -z_error) & (df['duration_std'] >= z_error)].index)].copy()
+	df = df[df.index.isin(df.loc[(df['duration_z'] < -z_error) | (df['duration_std'] < z_error)].index)].copy()
+
+	print(df_outliers.file)
+	print(df.file)
+
+	print("Number of spikes discarded:", df_outliers.groupby(['file','type']).count())
+	return df
